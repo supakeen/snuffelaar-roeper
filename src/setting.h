c71072c670d7847741420f5fd9280396a72baa43
supakeen <cmdr@supakeen.com>
2021-08-07 21:26:01 +0200

    Reorg.

---
+++ src/setting.h
@@ -0,0 +1,27 @@
+#ifndef FIRMWARE_NAME
+#define FIRMWARE_NAME "snuffelaarroeper"
+#endif
+
+#ifndef ROOM_NAME
+#define ROOM_NAME "bedroom"
+#endif
+
+#ifndef WIFI_SSID
+#define WIFI_SSID "x"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "x"
+#endif
+
+#ifndef MQTT_SERVER
+#define MQTT_SERVER "192.168.1.10"
+#endif
+
+#ifndef MQTT_PORT
+#define MQTT_PORT 1883
+#endif
+
+#ifndef OTA_PASSWORD
+#define OTA_PASSWORD "abcd"
+#endif
